58695ba37ea2e1687058cc5b17efd0e63b83ab6d
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-21 15:03:28 -0400

    add moment and only request new token once expired

--- src/adoptions/adoptions_router.js
+++ src/adoptions/adoptions_router.js
@@ -1,17 +1,20 @@
 'use strict';
 
+const qs = require('querystring');
+const moment = require('moment');
 const express = require('express');
 const adoptionsRouter = express.Router();
 const bodyParser = express.json();
 const UserQueue = require('../users/user_queue');
 const DogQueue = require('./dog_queue');
 const CatQueue = require('./cat_queue');
-const axios = require('axios').create({
-  baseURL: process.env.PETFINDER_API_URL,
-  headers: {'Authorization': `Bearer ${process.env.PETFINDER_API_TOKEN}`}
-});
+const axios = require('axios');
 const utils = require('../utils');
 
+const authToken = {
+  token: '',
+  expiresAt: moment()
+};
 
 function buildAnimalObj(res) {
   return {
@@ -23,14 +26,54 @@ function buildAnimalObj(res) {
   };
 }
 
+async function getAuthToken(req, res, next) {
+  const currentTime = moment();
+
+  if (currentTime < authToken.expiresAt) {
+    req.authToken = authToken;
+    return next();
+  }
+
+  const data = {
+    'grant_type': 'client_credentials',
+    'client_id': process.env.PETFINDER_CLIENT_ID,
+    'client_secret': process.env.PETFINDER_CLIENT_SECRET
+  };
+
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data),
+    url: 'https://api.petfinder.com/v2/oauth2/token',
+  };
+  
+  const response = await axios(options);
+  authToken.expiresAt = moment().add(response.data.expires_in, 's');
+  authToken.token = response.data.access_token;
+  req.authToken = authToken;
+  next();
+}
+
 
 adoptionsRouter
   .route('/')
-  .get(async (req, res, next) => {
+  .get(getAuthToken, async (req, res, next) => {
     const zipcode = req.params.zipcode || '95677';
+    const baseUrl = process.env.PETFINDER_API_URL;
 
-    const dogData = await axios.get(`/animals?type=dog&location=${zipcode}`);
-    const catData = await axios.get(`/animals?type=cat&location=${zipcode}`);
+    const dogRequest = axios.get(`${baseUrl}/animals?type=dog&location=${zipcode}`, {
+      headers: {
+        'Authorization': `Bearer ${req.authToken.token}`
+      }
+    });
+    const catRequest = axios.get(`${baseUrl}/animals?type=cat&location=${zipcode}`, {
+      headers: {
+        'Authorization': `Bearer ${req.authToken.token}`
+      }
+    });
+
+    const [dogData, catData] = await Promise.all([dogRequest, catRequest]);
+
     const dogs = dogData.data.animals.map(dogObj => {
       if (dogObj.photos.length) {
         return buildAnimalObj(dogObj);
@@ -45,11 +88,9 @@ adoptionsRouter
     dogs.forEach(dog => DogQueue.enqueue(dog));
     cats.forEach(cat => CatQueue.enqueue(cat));
     res.json({dog: utils.peek(DogQueue), cat: utils.peek(CatQueue)});
-    
-    // make request to dog api - clean the response
-    // make request to cat api
-    // 
   });
+
+
   
 
 
